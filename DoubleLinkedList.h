44d8e9c0be6595a5824c04635a5996a814234f85
Tomer <a@a.com>
2014-11-26 16:21:42 +0200

    Moved template class DoubleLinkedList implementation to the .h file

--- DoubleLinkedList.h
+++ DoubleLinkedList.h
@@ -1,6 +1,6 @@
 /****************************************************************************/
 /*                                                                          */
-/* DoubleLinkedList class header                                            */
+/* DoubleLinkedList template class implementation                           */
 /*                                                                          */
 /****************************************************************************/
 
@@ -15,29 +15,66 @@ class DoubleLinkedList {
 public:
     // Constructor - Creates a new empty list
     // Time complexity: O(1)
-    DoubleLinkedList();
+    DoubleLinkedList() : mHead(NULL), mSize(0) {}
 
     // Destructor
     // Time complexity: O(k) where k is the list's length
-    ~DoubleLinkedList();
+    ~DoubleLinkedList() {
+        // Free all nodes
+        while (mHead) {
+            mHead = current->next;
+            // Free data
+            delete current->data;
+            // Free the node struct itself
+            delete current;
+            current = mhead;
+        }
+    }
 
     // Insert an element as the new first element
     // Time complexity: O(1)
     // Throws bad_alloc on allocation error
-    void insertFront(const ValueType& elem);
+    void insertFront(const ValueType& elem) {
+        // Allocate a new node
+        Node* node = new Node;
+
+        // Allocate a copy of elem, to keep in our node
+        try {
+            node->data = new ValueType(elem);
+        } catch (const bad_alloc& e) {
+            delete node;
+            throw;
+        }
+
+        node->next = mHead;
+        node->prev = NULL;
+
+        // Insert the new node into the list and update mSize accordingly
+        mHead = node;
+        mSize++;
+    }
 
     // Get the first element
     // Time complexity: O(1)
     // Throws NoSuchNodeException
-    ValueType* getFront() const;
+    ValueType* getFront() const {
+        if (isEmpty()) {
+            throw NoSuchNodeException();
+        }
+        return mHead->data;
+    }
 
     // Get the first element
     // Time complexity: O(1)
-    unsigned int getSize() const;
+    unsigned int getSize() const {
+        return mSize;
+    }
 
     // Get the first element
     // Time complexity: O(1)
-    bool isEmpty() const;
+    bool isEmpty() const {
+        return (mSize == 0);
+    }
 
     // Not needed for this exercise
     //void remove(const ValueType& elem);
@@ -48,7 +85,21 @@ public:
     // Time complexity: O(k) where k is the list's length
     // Throws NoSuchNodeException if no node matching the condition is found
     template<typename Condition>
-    ValueType* getDataByPredicate(const Condition& condition, void* extra) const;
+    ValueType* getDataByPredicate(const Condition& condition,
+            void* extra) const {
+
+        // Iterate over all nodes until one matches the condition
+        Node* p = head;
+        while (p) {
+            if (condition(p->data, extra)) {
+                // Found it !
+                return p->data;
+            }
+            p = p->next;
+        }
+
+        throw NoSuchNodeException();
+    }
 
 
     // Exception classes
